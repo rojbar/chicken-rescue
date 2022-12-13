0431efaa86e8905e4ef325424669be3454cf132c
rojbar <55609284+rojbar@users.noreply.github.com>
2022-12-13 10:44:37 -0500

    Added wood (lg|md|sm) platform imgs, and changed background brightness to show better the plaftorms

--- src/LevelOne.ts
+++ src/LevelOne.ts
@@ -7,10 +7,16 @@ export default class LevelOne extends Phaser.Scene {
 
 	preload() {
 		this.load.image('background', 'background/2857_1280x720.jpg')
+		this.load.image('small_wood', 'assets/wood_small_platform.jpg')
+		this.load.image('medium_wood', 'assets/wood_medium_platform.jpg')
+		this.load.image('large_wood', 'assets/wood_large_platform.jpg')
 	}
 
 	create() {
 		this.add.image(0, 0, 'background').setOrigin(0, 0)
+		this.add.image(70,620,'small_wood').setOrigin(0,0)
+		this.add.image(300,620,'medium_wood').setOrigin(0,0)
+		this.add.image(600,620,'large_wood').setOrigin(0,0)
 
 	}
 }

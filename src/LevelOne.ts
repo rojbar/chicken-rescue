b61439fecc92703c640dc8a1711b72cb8c2997f8
rojbar <55609284+rojbar@users.noreply.github.com>
2022-12-13 11:16:21 -0500

    Added wood box img

--- src/LevelOne.ts
+++ src/LevelOne.ts
@@ -11,6 +11,7 @@ export default class LevelOne extends Phaser.Scene {
 		this.load.image('medium_wood', 'assets/wood_medium_platform.jpg')
 		this.load.image('large_wood', 'assets/wood_large_platform.jpg')
 		this.load.image('wall_wood', 'assets/wall_wood_platform.png')
+		this.load.image('wood_box', 'assets/wood_box.png')
 	}
 
 	create() {
@@ -18,7 +19,9 @@ export default class LevelOne extends Phaser.Scene {
 		this.add.image(70,620,'small_wood').setOrigin(0,0)
 		this.add.image(300,620,'medium_wood').setOrigin(0,0)
 		this.add.image(600,620,'large_wood').setOrigin(0,0)
+		this.add.image(40,165, 'wood_box').setOrigin(0,0)
 		this.add.image(0,200, 'wall_wood').setOrigin(0,0)
+		
 
 	}
 }

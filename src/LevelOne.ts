e8dce412688f3fc542093ec5f0dad970d41b5d46
Esteban Lopez <elopez@truora.com>
2023-01-08 19:21:44 -0500

    Add chicken controlls to jump and move left and right

--- src/LevelOne.ts
+++ src/LevelOne.ts
@@ -11,12 +11,12 @@ export default class LevelOne extends Phaser.Scene {
 
 	// cursors = this.input.keyboard.createCursorKeys()
 	platforms!: Phaser.Physics.Arcade.StaticGroup
-	eggs: integer 
+	eggs: integer
 	snakes!: Phaser.GameObjects.Sprite[]
 	chicken!: Chicken
 
-	init () {
-		this.chicken =  new Chicken(this)
+	init() {
+		this.chicken = new Chicken(this)
 	}
 
 	create() {
@@ -34,144 +34,148 @@ export default class LevelOne extends Phaser.Scene {
 		this.createRats()
 	}
 
-	private createSnakes(){
+	update() {
+		this.chicken.chickenControls()
+	}
+
+	private createSnakes() {
 		//this.snakes.push(this.add.sprite(130,660, TextureKeys.Snake))
-		const a = this.add.sprite(620,688, TextureKeys.Snake)
-		a.setScale(2.5,2.5)
+		const a = this.add.sprite(620, 688, TextureKeys.Snake)
+		a.setScale(2.5, 2.5)
 		a.play(AnimationKeys.SnakeIdle)
 		//a.play(AnimationKeys.SnakeDeath)
 
 	}
 
-	private createRats(){
+	private createRats() {
 		//this.snakes.push(this.add.sprite(130,660, TextureKeys.Snake))
-		const b = this.add.sprite(420,558, TextureKeys.Rat)
-		b.setScale(2.5,2.5)
+		const b = this.add.sprite(420, 558, TextureKeys.Rat)
+		b.setScale(2.5, 2.5)
 		b.play(AnimationKeys.RatWalking)
 		//a.play(AnimationKeys.SnakeDeath)
 
 	}
 
-	private initPlatforms(){
+	private initPlatforms() {
 		this.platforms = this.physics.add.staticGroup()
 		this.physics.add.collider(this.chicken.getPlayer(), this.platforms)
 	}
 
-	private createBackground(){
+	private createBackground() {
 		this.add.image(0, 0, TextureKeys.Background).setOrigin(0, 0)
 	}
 
-	private createFloor(){
+	private createFloor() {
 		let base = 180
 		for (let index = 0; index < 8; index++) {
-			const platform = this.platforms.create(base * (index),713,TextureKeys.LargeWoodTile)
+			const platform = this.platforms.create(base * (index), 713, TextureKeys.LargeWoodTile)
 			platform.body.updateFromGameObject()
-		}		
+		}
 	}
 
-	private createLevelPlatforms(){
+	private createLevelPlatforms() {
 		const firstRowX = 130
 		const firstRowY = 660
 
-		this.platforms.create(firstRowX,firstRowY,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+80,firstRowY-60,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+190,firstRowY-25,TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX, firstRowY, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 80, firstRowY - 60, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 190, firstRowY - 25, TextureKeys.SmallWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+340,firstRowY-80,TextureKeys.LargeWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+395,firstRowY-20,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		
-		this.platforms.create(firstRowX+510,firstRowY-150,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+720,firstRowY-150,TextureKeys.MediumWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 340, firstRowY - 80, TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 395, firstRowY - 20, TextureKeys.SmallWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+910,firstRowY,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+880,firstRowY-180,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+1010,firstRowY-230,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+880,firstRowY-300,TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 510, firstRowY - 150, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 720, firstRowY - 150, TextureKeys.MediumWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+640,firstRowY-290,TextureKeys.LargeWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+430,firstRowY-290,TextureKeys.LargeWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+215,firstRowY-290,TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 910, firstRowY, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 880, firstRowY - 180, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 1010, firstRowY - 230, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 880, firstRowY - 300, TextureKeys.SmallWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+35,firstRowY-325,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX-110,firstRowY-365,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+15,firstRowY-435,TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 640, firstRowY - 290, TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 430, firstRowY - 290, TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 215, firstRowY - 290, TextureKeys.LargeWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+175,firstRowY-460,TextureKeys.MediumWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+355,firstRowY-430,TextureKeys.LargeWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+575,firstRowY-470,TextureKeys.SmallWoodTile).body.updateFromGameObject()
-		this.platforms.create(firstRowX+795,firstRowY-430,TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 35, firstRowY - 325, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX - 110, firstRowY - 365, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 15, firstRowY - 435, TextureKeys.SmallWoodTile).body.updateFromGameObject()
 
-		this.platforms.create(firstRowX+1055,firstRowY-520,TextureKeys.SmallWoodTile)
-		this.platforms.create(firstRowX+1110,firstRowY-520,TextureKeys.SmallWoodTile)
+		this.platforms.create(firstRowX + 175, firstRowY - 460, TextureKeys.MediumWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 355, firstRowY - 430, TextureKeys.LargeWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 575, firstRowY - 470, TextureKeys.SmallWoodTile).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 795, firstRowY - 430, TextureKeys.LargeWoodTile).body.updateFromGameObject()
 
-		const wallWoodImage = this.add.image(firstRowX+1085,firstRowY-500,TextureKeys.WallWoodTile)
+		this.platforms.create(firstRowX + 1055, firstRowY - 520, TextureKeys.SmallWoodTile)
+		this.platforms.create(firstRowX + 1110, firstRowY - 520, TextureKeys.SmallWoodTile)
+
+		const wallWoodImage = this.add.image(firstRowX + 1085, firstRowY - 500, TextureKeys.WallWoodTile)
 		wallWoodImage.flipX = !wallWoodImage.flipX
 
-		const lastSmall = this.platforms.create(firstRowX+952,firstRowY-470,TextureKeys.SmallWoodTile)
+		const lastSmall = this.platforms.create(firstRowX + 952, firstRowY - 470, TextureKeys.SmallWoodTile)
 		lastSmall.setScale(0.5)
-		lastSmall.body.updateFromGameObject()		
+		lastSmall.body.updateFromGameObject()
 	}
 
-	private createLevelBoxes(){
+	private createLevelBoxes() {
 		const firstRowX = 130
 		const firstRowY = 680
 
-		this.platforms.create(firstRowX+230,firstRowY,TextureKeys.WoodBox).body.updateFromGameObject()
-		this.platforms.create(firstRowX+408,firstRowY-132,TextureKeys.WoodBox).body.updateFromGameObject()
-		this.platforms.create(firstRowX+550,firstRowY,TextureKeys.WoodBox).body.updateFromGameObject()
-		this.platforms.create(firstRowX+595,firstRowY,TextureKeys.WoodBox).body.updateFromGameObject()
-		this.platforms.create(firstRowX+640,firstRowY,TextureKeys.WoodBox).body.updateFromGameObject()
-		this.platforms.create(firstRowX+215,firstRowY-342,TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 230, firstRowY, TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 408, firstRowY - 132, TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 550, firstRowY, TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 595, firstRowY, TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 640, firstRowY, TextureKeys.WoodBox).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 215, firstRowY - 342, TextureKeys.WoodBox).body.updateFromGameObject()
 	}
 
-	private createLevelBreakableBoxes(){
+	private createLevelBreakableBoxes() {
 		const firstRowX = 130
 		const firstRowY = 680
-		this.platforms.create(firstRowX+685,firstRowY,TextureKeys.WoodBoxBreakable).body.updateFromGameObject()
-		this.platforms.create(firstRowX+1125,firstRowY,TextureKeys.WoodBoxBreakable).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 685, firstRowY, TextureKeys.WoodBoxBreakable).body.updateFromGameObject()
+		this.platforms.create(firstRowX + 1125, firstRowY, TextureKeys.WoodBoxBreakable).body.updateFromGameObject()
 	}
 
-	private createLevelRakes(){
+	private createLevelRakes() {
 		const firstRowX = 845
 		const firstRowY = 485
-		this.platforms.create(firstRowX-38,firstRowY,TextureKeys.Rake).body.updateFromGameObject()
-		
+		this.platforms.create(firstRowX - 38, firstRowY, TextureKeys.Rake).body.updateFromGameObject()
+
 		const secondRowX = 1000
 		const secondRowY = 688
-		this.platforms.create(secondRowX,secondRowY,TextureKeys.Rake).body.updateFromGameObject()
-		this.platforms.create(secondRowX+80,secondRowY,TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(secondRowX, secondRowY, TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(secondRowX + 80, secondRowY, TextureKeys.Rake).body.updateFromGameObject()
 
 		const thirdRowX = 630
 		const thirdRowY = 345
-		this.platforms.create(thirdRowX+3,thirdRowY,TextureKeys.Rake).body.updateFromGameObject()
-		this.platforms.create(thirdRowX+180,thirdRowY,TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(thirdRowX + 3, thirdRowY, TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(thirdRowX + 180, thirdRowY, TextureKeys.Rake).body.updateFromGameObject()
 
-		this.platforms.create(thirdRowX-630,thirdRowY-75,TextureKeys.Rake).body.updateFromGameObject()
-		this.platforms.create(thirdRowX-280,thirdRowY-170,TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(thirdRowX - 630, thirdRowY - 75, TextureKeys.Rake).body.updateFromGameObject()
+		this.platforms.create(thirdRowX - 280, thirdRowY - 170, TextureKeys.Rake).body.updateFromGameObject()
 	}
 
-	private createEggs(){
-		const egg1 = this.platforms.create(210,578,TextureKeys.CollectedEgg)
+	private createEggs() {
+		const egg1 = this.platforms.create(210, 578, TextureKeys.CollectedEgg)
 		egg1.setScale(0.65)
 		egg1.body.updateFromGameObject()
 
-		const egg2 = this.platforms.create(525,618,TextureKeys.CollectedEgg)
+		const egg2 = this.platforms.create(525, 618, TextureKeys.CollectedEgg)
 		egg2.setScale(0.65)
 		egg2.body.updateFromGameObject()
 
-		const egg3 = this.platforms.create(1255,680,TextureKeys.CollectedEgg)
+		const egg3 = this.platforms.create(1255, 680, TextureKeys.CollectedEgg)
 		egg3.setScale(0.65)
 		egg3.body.updateFromGameObject()
 
-		const egg4 = this.platforms.create(1010,338,TextureKeys.CollectedEgg)
+		const egg4 = this.platforms.create(1010, 338, TextureKeys.CollectedEgg)
 		egg4.setScale(0.65)
 		egg4.body.updateFromGameObject()
 
-		const egg5 = this.platforms.create(30,272,TextureKeys.CollectedEgg)
+		const egg5 = this.platforms.create(30, 272, TextureKeys.CollectedEgg)
 		egg5.setScale(0.65)
 		egg5.body.updateFromGameObject()
 
-		const egg6 = this.platforms.create(705,166,TextureKeys.CollectedEgg)
+		const egg6 = this.platforms.create(705, 166, TextureKeys.CollectedEgg)
 		egg6.setScale(0.65)
 		egg6.body.updateFromGameObject()
 	}
--- src/characters/chicken.ts
+++ src/characters/chicken.ts
@@ -1,31 +1,39 @@
 export class Chicken {
     relatedScene: Phaser.Scene;
     player!: Phaser.Physics.Arcade.Sprite;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    spacebar: Phaser.Input.Keyboard.Key;
 
     constructor(scene: Phaser.Scene) {
         this.relatedScene = scene;
+        this.cursors = this.relatedScene.input.keyboard.createCursorKeys();
+        this.spacebar = this.relatedScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     create() {
         this.player = this.relatedScene.physics.add.sprite(500, 630, 'chicken');
+        this.player.setCollideWorldBounds(true);
     }
 
     getPlayer() {
         return this.player;
     }
 
-    // private chickenControls(){
-	// 	if (this.cursors.left.isDown){
-	// 		this.chicken.setVelocityX(-160)
-	// 		this.chicken.anims.play(AnimationKeys.ChickenLeft, true)
-	// 	} else if (this.cursors.right.isDown){
-	// 		this.chicken.setVelocityX(160)
-	// 		this.chicken.anims.play(AnimationKeys.ChickenRight, true)
-	// 	} else {
-	// 		this.chicken.setVelocityX(0)
-	// 		this.chicken.anims.play(AnimationKeys.ChickenIdle, true)
-	// 	}
-	// }
+    chickenControls() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160)
+            //	this.chicken.anims.play(AnimationKeys.ChickenLeft, true)
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160)
+            //	this.chicken.anims.play(AnimationKeys.ChickenRight, true)
+        } else if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+            this.player.setVelocity(0, -350);
+        }
+        else {
+            this.player.setVelocityX(0)
+            //	this.chicken.anims.play(AnimationKeys.ChickenIdle, true)
+        }
+    }
 
 
 }
